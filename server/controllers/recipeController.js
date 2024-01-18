315e5ea97b9401b4f01f1377ba2d987171e00f7a
kalash8 <kalashtelkar@gmail.com>
2024-01-19 03:34:01 +0530

    category schema and setting up mongodb

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,5 @@
-
-
+require('../models/database')
+const category = require('../models/category')
 
 /**
  * GET /
@@ -7,5 +7,58 @@
 */
 
 exports.homepage = async(req, res) => {
-    res.render('index', { title: 'Cooking Blog - Home' } );
+
+    try {
+
+        const limitNumber = 5;
+        const categories = await category.find({}).limit(limitNumber);
+        res.render('index', { title: 'Cooking Blog - Home', categories } );
+    } catch (error) {
+        res.satus(500).send({message: error.message || "Error Occured"})
+    }
+
+
+    
 }
+
+/**
+ * async function insertDummyCategoryData(){
+    try {
+        await category.insertMany([
+            {
+                "name": "Thai",
+                "image": "thai-food.jpg"
+            },
+            {
+                "name": "American",
+                "image": "american-food.jpg"
+            },
+            {
+                "name": "Chinese",
+                "image": "Chinese-food.jpg"
+            },
+            {
+                "name": "Mexican",
+                "image": "mexican-food.jpg"
+            },
+            {
+                "name": "Indian",
+                "image": "indian-food.jpg"
+            },
+            {
+                "name": "Spanish",
+                "image": "spanish-food.jpg"
+            },
+        
+        
+        ]);
+    } catch (error) {
+        console.log('err', + error)
+    }
+}
+ */
+
+
+
+
+/**insertDummyCategoryData(); */
---
+++ server/models/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log('Connected');
+
+});
+
+
+//Models
+require('./category');
